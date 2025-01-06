07861abbe197da65ee1d828564bd4b129e5b2b56
Omprasadbl <omprasadbl@gmail.com>
2025-01-06 23:36:53 +0530

    Created Card for todo list content in Home page

--- frontend/note-app/src/components/navbar/Navbar.jsx
+++ frontend/note-app/src/components/navbar/Navbar.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react'
-import ProfileInfo from '../Cards/ProfileInfo'
-import { useNavigate } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
+import React, { useState } from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
-  const [searchQuery,setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const navigate= useNavigate
-  const onLogout=()=>{
-    navigate('/login')
-
-  }
-  const handleSearch=()=>{}
-  const onClearSearch=(e)=>setSearchQuery("")
+  const navigate = useNavigate;
+  const onLogout = () => {
+    navigate("/login");
+  };
+  const handleSearch = () => {};
+  const onClearSearch = (e) => setSearchQuery("");
   return (
-
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-      <h2 className=' text-xl font-medium text-black py-2'>Notes</h2>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className=" text-xl font-medium text-black py-2">Notes</h2>
       <SearchBar
         value={searchQuery}
-        onChange={({target})=>{
-          setSearchQuery(target.value)
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
         }}
         handleSearch={handleSearch}
         onClearSearch={onClearSearch}
       />
-      <ProfileInfo onLgout={onLogout}/>
+      <ProfileInfo onLgout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/note-app/src/pages/Home/Home.jsx
+++ frontend/note-app/src/pages/Home/Home.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import NoteCard from '../../components/Cards/NoteCard'
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar/>
+    <>
+      <Navbar />
       <div className="container mx-auto">
-        <NoteCard/>
+      <div className="grid  grid-cols-3  gap-4 mt-8">
+        <NoteCard
+          title="Meeting today"
+          date="6 jab 26"
+          content="meeting on 7th jan  "
+          tags="#meeting  "
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
+
+<NoteCard
+          title="Meeting today"
+          date="6 jab 26"
+          content="meeting on 7th jan  "
+          tags="#meeting  "
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
+
+<NoteCard
+          title="Meeting today"
+          date="6 jab 26"
+          content="meeting on 7th jan  "
+          tags="#meeting  "
+          isPinned={true}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
       </div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Home
-
+export default Home;
