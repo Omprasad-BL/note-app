7b7e30e0e1ec6d65a938294faa8596f00673cbf9
Omprasadbl <omprasadbl@gmail.com>
2025-01-15 22:57:06 +0530

    server side login router created

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,9 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/create-account"), async (req, res) => {
+app.post("/create-account", async (req, res) => {
   const { fullName, email, password } = req.body;
+//   return res.status(200).json("working")
   if (!fullName)
     return res.status(404).json({ error: true, message: "Fullname is required" });
 
@@ -62,7 +63,40 @@ return res.status(200).json({
     message: "Registration successfully"
 })
 }
+)
 
+app.post('/login',async (req,res) => {
+  const { email, password } = req.body;
+  if(!email){
+    return res.status(404).json({ error: true, message: "Email is required" });
+  }
+
+  if(!password){
+    return res.status(404).json({ error: true, message: "Password is required" });
+  }
+
+  const userInfo=await User.findOne({ email: email})
+
+  if(!userInfo){
+    return res.status(401).json({  message: "User not found" });
+  }
+
+  if(userInfo.email == email&& userInfo.password==password){
+    const user={
+      user:userInfo
+    }
+    const accessToken=jwt.sign(user , process.env.ACCESS_TOKEN_SECRET,{
+      expiresIn: "1h"
+    })
+    return res.json({error:false,
+      message:"Login successfull",
+      email,
+      accessToken
+    })
+  }  else{
+    return res.json({error:true,message:"Invalid credentials"});
+  }
+})
 app.get("/users", (req, res) => {
   res.json({ name: "Omprasad B L", age: 23, address: "Shivamoga  " });
 });
--- backend/utilities.js
+++ backend/utilities.js
@@ -3,4 +3,15 @@ const jwt= require('jsonwebtoken')
 function authenticationToken(req, res, next) {
     const authHeader = req.headers["Authorization"]
     const token = authHeader && authHeader.split(" ")[1]
+    if(!token) return res.sendStatus(401)
+    
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,next) => {
+        if(err) return res.sendStatus(401)
+        req.user=user;
+        next();
+    })
+}
+
+module.exports = {
+    authenticationToken
 }
